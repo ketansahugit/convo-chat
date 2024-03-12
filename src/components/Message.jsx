8e1f66680ce2fc3e15307d3b836190ab08895849
Ketan Sahu <rorozoro9888@gmail.com>
2024-03-12 22:31:31 +0530

    add message frontend

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Message = () => {
   return (
-    <div>Message</div>
+    <div className='message owner'>
+      <div className="messageInfo">
+        <img src="https://images.pexels.com/photos/19567574/pexels-photo-19567574/free-photo-of-man-and-woman-near-pond-together.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+        <span>Just now</span>
+      </div>
+      <div className="messageContent">
+        <p>hello</p>
+       <img src="https://images.pexels.com/photos/19567574/pexels-photo-19567574/free-photo-of-man-and-woman-near-pond-together.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" /> 
+      </div>
+    </div>
   )
 }
 
